dbb4886dda4e12ce8316ef58339414e5183371e2
Baldeep Kang <bkang4612@gmail.com>
2020-12-14 13:03:55 -0800

    Restructure folder and update Main

---
+++ employee-directory/src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+//import "./styles/Main.css";
+import Container from "../Container/Container";
+
+export default function Main(){
+  
+    return (
+      <>
+        <Container />
+      </>
+    );
+  
+}
