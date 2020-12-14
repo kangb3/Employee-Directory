bb60042cba50103a18e2c2ee79fc92187ec769a7
Baldeep Kang <bkang4612@gmail.com>
2020-12-14 13:07:33 -0800

    update App.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
-import Main from "./components/Main";
-import Wrapper from "./components/Wrapper";
-import Header from "./components/Header";
+import Main from "./components/Main/Main";
+import Header from "./components/Header/Header";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Wrapper>
         <Header />
         <Main />
-      </Wrapper>
     </div>
   );
 }
